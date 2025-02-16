1e75fbe5ee876fe131e1b0c556c3b2f8f828b43a
AMAR DEEP s <138798428+amargauge26@users.noreply.github.com>
2025-02-16 16:27:18 +0530

    Update settings.py

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -171,3 +171,6 @@ EMAIL_HOST_PASSWORD = 'hfqjknkonjknovjknkjljglydkbkjjej'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+
